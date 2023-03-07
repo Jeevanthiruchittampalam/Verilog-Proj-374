6b740e0293b938084e4dc71f761cd9555333255a
Jeevanthiruchittampalam <113857754+Jeevanthiruchittampalam@users.noreply.github.com>
2023-03-06 19:43:04 -0500

    Update divider.v

--- divider.v
+++ divider.v
@@ -1,12 +1,33 @@
-module divider(input signed [31:0] dividend, divisor, output reg [32*2-1:0] z);
-	reg [63:32] high, low;
-	always @ (*)
+module divider(A,B,Result);
+
+input [31:0] A;
+input [31:0] B;
+output [31:0] Result;
+
+reg [31:0] Result = 0;
+reg [31:0] a1,b1;
+reg [32:0] p1;
+integer i;
+
+always@ (A or B)
+begin
+	a1 = A;
+	b1 = B;
+	p1 = 0;
+	for(i=0;i<32;i=i+1)
 	begin
-		high = dividend % divisor;
-		low = (dividend - high) / divisor;
+		p1 = {p1[30:0],a1[31]};
+		a1[31:1] = a1[30:0];
+		p1 = p1-b1;
+		if(p1[31]==1'b1)
 		begin
-			z = {high, low};
+			a1[0] = 1'b0;
+			p1 = p1+b1;
 		end
+		else
+			a1[0] = 1'b1;
 	end
-				
+	Result = a1;
+end
+
 endmodule
