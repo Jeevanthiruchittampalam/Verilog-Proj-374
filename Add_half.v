4a6dce1cdfe324ea5ade199838e87585f8adca74
Jeevan Thiruchittampalam <19jt20@queensu.ca>
2023-03-10 14:13:04 -0500

    wefwffewsf::

--- Add_half.v
+++
@@ -1,6 +0,0 @@
-`timescale 1ns/10ps
-
-module Add_half (output c_out, sum, input a, b);
-	xor (sum, a, b);
-	and (c_out, a, b);
-endmodule
--- divider.v
+++ divider.v
@@ -1,33 +1,33 @@
-module divider(
-  input wire clk,
-  input wire rst,
-  input wire [31:0] dividend,
-  input wire [31:0] divisor,
-  output reg [31:0] quotient
-);
-  reg [31:0] dividend_temp = 0;
-  reg [31:0] divisor_temp = 0;
-  reg [31:0] bit_temp = 0;
-  integer i;
+module divider(A,B,Result);
 
-  always @(posedge clk) begin
-    if (rst) begin
-      dividend_temp <= 0;
-      divisor_temp <= 0;
-      bit_temp <= 0;
-      quotient <= 0;
-    end else begin
-      dividend_temp <= dividend;
-      divisor_temp <= divisor;
-      bit_temp <= {bit_temp[30:0],1'b0};
-      for (i=0; i<32; i=i+1) begin
-        if (dividend_temp >= divisor_temp) begin
-          dividend_temp <= dividend_temp - divisor_temp;
-          quotient <= quotient + bit_temp;
-        end
-        divisor_temp <= {1'b0, divisor_temp[31:1]};
-        bit_temp <= {1'b0, bit_temp[31:1]};
-      end
-    end
-  end
+input [31:0] A;
+input [31:0] B;
+output [31:0] Result;
+
+reg [31:0] Result = 0;
+reg [31:0] a1,b1;
+reg [32:0] p1;
+integer i;
+
+always@ (A or B)
+begin
+	a1 = A;
+	b1 = B;
+	p1 = 0;
+	for(i=0;i<32;i=i+1)
+	begin
+		p1 = {p1[30:0],a1[31]};
+		a1[31:1] = a1[30:0];
+		p1 = p1-b1;
+		if(p1[31]==1'b1)
+		begin
+			a1[0] = 1'b0;
+			p1 = p1+b1;
+		end
+		else
+			a1[0] = 1'b1;
+	end
+	Result = a1;
+end
+
 endmodule
